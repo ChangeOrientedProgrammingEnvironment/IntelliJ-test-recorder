0025d4ba53e47d4e1d322a9399fc8671e6052706
Nicholas Nelson <nelsonni@oregonstate.edu>
2015-11-18 21:25:31 -0800

    NodeActivityTracker is a Singleton class to aggregate running totals of AST nodes modified (added, moved, deleted) within the context of PsiTreeChangeEvent events.

---
+++ test-recorder/src/edu/oregonstate/cope/core/NodeActivityTracker.java
@@ -0,0 +1,55 @@
+package edu.oregonstate.cope.core;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.util.PsiTreeUtil;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Change-Oriented Programming Environment (COPE) project
+ * URL: http://cope.eecs.oregonstate.edu/
+ * Created by nelsonni on 11/18/15.
+ */
+public final class NodeActivityTracker {
+
+    private static NodeActivityTracker instance = null;
+    private Map<String, Integer> methodMap = new HashMap<>();
+
+    private NodeActivityTracker() {
+        // Exists only to defeat instantiation.
+    }
+
+    public static NodeActivityTracker getInstance() {
+        if(instance == null) {
+            instance = new NodeActivityTracker();
+        }
+        return instance;
+    }
+
+    public Integer get(PsiMethod method) {
+        return methodMap.get(method.getName());
+    }
+
+    public void update(PsiMethod method, Integer count) {
+        String key = method.getName();
+        Integer old = methodMap.getOrDefault(key, 0);
+        methodMap.remove(key);
+        methodMap.put(key, (old + count));
+    }
+
+//    public void collectMethods(Project project, VirtualFile vFile) {
+//        PsiFile pfile = PsiManager.getInstance(project).findFile(vFile);
+//        PsiElement root = pfile.findElementAt(0);
+//
+//        Collection<PsiMethod> methods = PsiTreeUtil.findChildrenOfType(root, PsiMethod.class);
+//        for (PsiMethod method : methods) { update(method, 0); };
+//    }
+
+}
